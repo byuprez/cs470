521ca509f68a430f5efa58f7469056e5e2d12792
Tim Major <teh.rabid.hamster@gmail.com>
2011-09-29 22:55:22 -0600

    pf agent is behaving rather well.  Fixed problems with normalization of turning angles.  Ready to play against really dumb agent.

--- pfagent.py
+++ pfagent.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import sys
-import math
-import time
+import sys, math, time
 from mybzrc import MyBZRC, Command
 
 class BZRException(Exception): pass
 
 #Variables for frobbing the PD controller
-PD_Kp = 2
+PD_Kp = 3
 PD_Kd = 2
 
 #Base class of all objects in our world that can produce a potential field,
@@ -18,14 +16,63 @@ class FieldGenerator:
     def generate_field(self, x, y):
         pass
         
-    def generate_attractive(self, x, y):
-        pass
+    def calc_dist(self, x, y, goal_x, goal_y):
+        return math.sqrt((goal_x - x)**2 + (goal_y - y)**2)
         
-    def generate_repulsive(self, x, y):
-        pass
+    def calc_theta(self, x, y, goal_x, goal_y):
+        return math.atan2(goal_y - y, goal_x - x)
         
-    def generate_tangential(self, x, y):
-        pass
+    def generate_attractive(self, x, y, goal_x, goal_y, alpha, radius, spread):
+        dist = self.calc_dist(x, y, goal_x, goal_y)
+        theta = self.calc_theta(x, y, goal_x, goal_y)
+        
+        if dist < radius:
+            return 0,0
+            
+        elif radius <= dist and dist <= spread + radius:
+            x = alpha * (dist - radius) * math.cos(theta)
+            y = alpha * (dist - radius) * math.sin(theta)
+            return x,y
+            
+        elif dist > spread + radius:
+            x = alpha * spread * math.cos(theta)
+            y = alpha * spread * math.sin(theta)
+            return x,y
+        
+    def generate_repulsive(self, x, y, goal_x, goal_y, beta, radius, spread):
+        dist = self.calc_dist(x, y, goal_x, goal_y)
+        theta = self.calc_theta(x, y, goal_x, goal_y)
+        
+        if dist < radius:
+            x = -(beta*(math.cos(theta))*float(10000))
+            y = -(beta*(math.sin(theta))*float(10000))
+            return x,y
+            
+        elif radius <= dist and dist <= spread + radius:
+            x = -(beta*(spread + radius - dist)*math.cos(theta))
+            y = -(beta*(spread + radius - dist)*math.sin(theta))
+            return x,y
+            
+        elif dist > radius + spread:
+            return 0,0
+        
+    def generate_tangential(self, x, y, goal_x, goal_y, gamma, radius, spread):
+        dist = self.calc_dist(x, y, goal_x, goal_y)
+        #Modify the theta to produce a perpendicular field
+        theta = self.calc_theta(x, y, goal_x, goal_y) + (math.pi / 2)
+        
+        if dist < radius:
+            x = -(gamma*(math.cos(theta))*float(100))
+            y = -(gamma*(math.sin(theta))*float(100))
+            return x,y
+            
+        elif radius <= dist and dist <= spread + radius:
+            x = -(gamma*(spread + radius - dist)*math.cos(theta))
+            y = -(gamma*(spread + radius - dist)*math.sin(theta))
+            return x,y
+            
+        elif dist > radius + spread:
+            return 0,0
 
 
 class EnemyTank(FieldGenerator):
@@ -36,13 +83,16 @@ class EnemyTank(FieldGenerator):
         self.pos_y = tank.y
         self.radius = radius
         #These variables can be used for frobbing the field
-        self.spread = 0
+        self.spread = 100
         self.alpha = 0 #Attractive field strength
-        self.beta = 0 #Repulsive field strength
-        self.gamma = 0 #Tangential field strength
+        self.beta = .5 #Repulsive field strength
+        self.gamma = .5 #Tangential field strength
         
     def generate_field(self, x, y):
-        return 0,0
+        repulsive_x, repulsive_y = self.generate_repulsive(x,y,self.pos_x,self.pos_y,self.beta,self.radius, self.spread)
+        tangential_x, tangential_y = self.generate_tangential(x,y,self.pos_x,self.pos_y,self.gamma,self.radius, self.spread)
+        
+        return (repulsive_x + tangential_x), (repulsive_y + tangential_y) 
         
         
 class FriendlyTank(FieldGenerator):
@@ -53,13 +103,16 @@ class FriendlyTank(FieldGenerator):
         self.pos_y = tank.y
         self.radius = radius
         #These variables can be used for frobbing the field
-        self.spread = 0
+        self.spread = 50
         self.alpha = 0 #Attractive field strength
-        self.beta = 0 #Repulsive field strength
-        self.gamma = 0 #Tangential field strength
+        self.beta = .5 #Repulsive field strength
+        self.gamma = 1 #Tangential field strength
         
     def generate_field(self, x, y):
-        return 0,0
+        repulsive_x, repulsive_y = self.generate_repulsive(x,y,self.pos_x,self.pos_y,self.beta,self.radius, self.spread)
+        tangential_x, tangential_y = self.generate_tangential(x,y,self.pos_x,self.pos_y,self.gamma,self.radius, self.spread)
+
+        return (repulsive_x + tangential_x), (repulsive_y + tangential_y)
         
         
 class EnemyBase(FieldGenerator):
@@ -93,36 +146,19 @@ class FriendlyBase(FieldGenerator):
         self.corner4_x = base.corner4_x
         self.corner4_y = base.corner4_y
         #These variables can be used for frobbing the field
-        self.spread = 100
-        self.alpha = 1.5 #Attractive field strength
-        self.beta = 0 #Repulsive field strength
-        self.gamma = 0 #Tangential field strength
+        self.spread = 10
+        self.alpha = 1 #Attractive field strength
         
     def generate_field(self, x, y):
-        return self.generate_attractive_field(x,y)
-        
-    def generate_attractive_field(self, x, y):
+        #Calculate the center of the base and its width
         width = self.corner1_x - self.corner4_x
         height = self.corner1_y - self.corner2_y
         center_x = self.corner1_x - (width / 2)
         center_y = self.corner1_y - (height / 2)
-        radius = min(width,height)
+        #
+        radius = (min(width,height) / 2)
         
-        dist = math.sqrt((center_x - x)**2 + (center_y - y)**2)
-        theta = math.atan2(center_y - y, center_x - x)
-        
-        if dist < radius:
-            return 0,0
-            
-        elif radius <= dist and dist <= self.spread + radius:
-            x = self.alpha * (dist - radius) * math.cos(theta)
-            y = self.alpha * (dist - radius) * math.sin(theta)
-            return x,y
-            
-        elif dist > self.spread + radius:
-            x = self.alpha * self.spread * math.cos(theta)
-            y = self.alpha * self.spread * math.sin(theta)
-            return x,y
+        return self.generate_attractive(x, y ,center_x, center_y, self.alpha, radius, self.spread)
         
         
 class EnemyFlag(FieldGenerator):
@@ -132,30 +168,12 @@ class EnemyFlag(FieldGenerator):
         self.poss_team = flag.poss_color
         self.radius = radius
         #These variables can be used for frobbing the field
-        self.spread = 100
-        self.alpha = 1.5 #Attractive field strength
-        self.beta = 0 #Repulsive field strength
-        self.gamma = 0 #Tangential field strength
+        self.spread = 50
+        self.alpha = 1 #Attractive field strength
         
     def generate_field(self, x, y):
-        return self.generate_attractive(x,y)
+        return self.generate_attractive(x, y, self.pos_x, self.pos_y, self.alpha, self.radius, self.spread)
         
-    def generate_attractive(self, x, y):
-        dist = math.sqrt((self.pos_x - x)**2 + (self.pos_y - y)**2)
-        theta = math.atan2(self.pos_y - y, self.pos_x - x)
-        
-        if dist < self.radius:
-            return 0,0
-            
-        elif self.radius <= dist and dist <= self.spread + self.radius:
-            x = self.alpha * (dist - self.radius) * math.cos(theta)
-            y = self.alpha * (dist - self.radius) * math.sin(theta)
-            return x,y
-            
-        elif dist > self.spread + self.radius:
-            x = self.alpha * self.spread * math.cos(theta)
-            y = self.alpha * self.spread * math.sin(theta)
-            return x,y
         
 class FriendlyFlag(FieldGenerator):
     def __init__(self, flag, radius):
@@ -179,26 +197,40 @@ class Shot(FieldGenerator):
         self.pos_y = shot.y
         self.radius = radius
         #These variables can be used for frobbing the field
-        self.spread = 0
+        self.spread = 100
         self.alpha = 0 #Attractive field strength
-        self.beta = 0 #Repulsive field strength
-        self.gamma = 0 #Tangential field strength
+        self.beta = 100 #Repulsive field strength
+        self.gamma = 1 #Tangential field strength
         
     def generate_field(self, x, y):
-        return 0,0
+        repulsive_x, repulsive_y = self.generate_repulsive(x,y,self.pos_x,self.pos_y,self.beta,self.radius, self.spread)
+        tangential_x, tangential_y = self.generate_tangential(x,y,self.pos_x,self.pos_y,self.gamma,self.radius, self.spread)
+
+        return (repulsive_x + tangential_x), (repulsive_y + tangential_y)
         
         
 class Obstacle(FieldGenerator):
     def __init__(self, obstacle):
         self.corners = obstacle
         #These variables can be used for frobbing the field
-        self.spread = 0
-        self.alpha = 0 #Attractive field strength
-        self.beta = 0 #Repulsive field strength
-        self.gamma = 0 #Tangential field strength
+        self.radius = 50
+        self.spread = 10
+        self.beta = .5 #Repulsive field strength
+        self.gamma = .3 #Tangential field strength
         
     def generate_field(self, x, y):
-        return 0,0
+        return_x = 0
+        return_y = 0
+        
+        #For each corner of the obstacle, create a tangential field
+        #to direct the agent around the obstacle
+        for corner in self.corners:
+            repulsive_x, repulsive_y = self.generate_repulsive(x,y,corner[0],corner[1],self.beta,self.radius, self.spread)
+            tangential_x, tangential_y = self.generate_tangential(x,y,corner[0],corner[1],self.gamma,self.radius, self.spread)
+            return_x += repulsive_x + tangential_x
+            return_y += repulsive_y + repulsive_y
+        
+        return return_x, return_y
 
 
 class PFAgent:
@@ -212,14 +244,17 @@ class PFAgent:
         print "Game constants: {0}".format(self.constants)
         
         #The only internal state that we are allowed to keep is the "previous" error for the PD controller
-        self.pd_error = 0
-        
+        self.angvel_pd_error = 0
+        self.vel_pd_error = 0
         
     #A moment of time has passed.  Calculate the potential field for where we are at, and perform an action
     #based on the field
     def tick(self, time_diff):
         #Find out information about myself
         myself = self.bzrc.get_mytanks()[self.tank_index]
+        
+        if myself.status == "dead":
+            return
         
         #Calculate the potential field at my location
         field_x, field_y = self.calculate_pf(myself.x, myself.y)
@@ -230,11 +265,19 @@ class PFAgent:
     #Applies a PD controller to a goal, and returns the action and new error.
     def pd_controller(self, goal, cur_value, prev_error, dt, kp, kd):
         #a = kp * (goal - cur_value) + kd * ((goal - cur_value) - prev_error)/dt
-        cur_error = goal - cur_value
+        cur_error = self.normalize_angle(goal - cur_value)
         derivative = (cur_error - prev_error) / dt
         result = (kp * cur_error) + (kd * derivative)
         return (result, cur_error)
-        
+    
+    def normalize_angle(self, angle):
+        """Make any angle be between +/- pi."""
+        angle -= 2 * math.pi * int (angle / (2 * math.pi))
+        if angle <= -math.pi:
+            angle += 2 * math.pi
+        elif angle > math.pi:
+            angle -= 2 * math.pi
+        return angle
         
     #Combines all of the behaviors/fields to produce a potential field for the agent's current position
     #If the agent has the flag, the goal is a friendly base, otherwise it is an enemy flag.  The 
@@ -260,7 +303,7 @@ class PFAgent:
         mytanks, othertanks, flags, bases, obstacles, shots = self.bzrc.get_lots_o_stuff()
         myself = mytanks[self.tank_index]
         sub_fields = []
-
+        
         #Seek goal behavior differs if we are holding the flag
         if myself.flag != "-":
             #I am holding a flag.  Set the goal to return to my base by creating a FriendlyBase field generator
@@ -273,9 +316,10 @@ class PFAgent:
             #I don't have a flag.  Set the goal to be an enemy flag by creating a EnemyFlag field generator for
             #each enemy flag
             for flag in flags:
-                if flag.color != self.constants['team']:
+                if flag.color == "blue":
                     sub_fields.append(EnemyFlag(flag, float(self.constants['flagradius'])))
                     break
+                    
 
         #Add in fields for enemy tanks
         for tank in othertanks:
@@ -292,7 +336,7 @@ class PFAgent:
 
         #Avoid shots
         for shot in shots:
-            subFields.append(Shot(shot, float(self.constants['shotradius'])))
+            sub_fields.append(Shot(shot, float(self.constants['shotradius'])))
 
         return sub_fields   
         
@@ -305,7 +349,9 @@ class PFAgent:
         #Calculate the velocity and angular velocity to do
         velocity = math.sqrt(delta_x**2 + delta_y**2)
         theta = math.atan2(delta_y, delta_x)
-        angvel, self.pd_error = self.pd_controller(theta, myself.angle, self.pd_error, time_diff, PD_Kp, PD_Kd)
+        
+        #Run the angle through a pd controller to get angular velocity
+        angvel, self.angvel_pd_error = self.pd_controller(theta, myself.angle, self.angvel_pd_error, time_diff, PD_Kp, PD_Kd)
         
         print "Command: velocity: {0} angvel: {1}".format(velocity, angvel)
         command = [Command(self.tank_index, velocity, angvel, False)]
